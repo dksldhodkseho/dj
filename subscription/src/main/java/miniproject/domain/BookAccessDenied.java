1c819a81e4b849b35c4d6496ac22217402b05284
Ji Yongju <156566854+dksldhodkseho@users.noreply.github.com>
2025-07-02 18:53:03 +0900

    commit

---
+++ subscription/src/main/java/miniproject/domain/BookAccessDenied.java
@@ -0,0 +1,25 @@
+package miniproject.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import miniproject.domain.*;
+import miniproject.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class BookAccessDenied extends AbstractEvent {
+
+    private Long userId;
+    private String bookId;
+
+    public BookAccessDenied(Subscription aggregate) {
+        super(aggregate);
+    }
+
+    public BookAccessDenied() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
---
+++ subscription/src/main/java/miniproject/domain/BookAccessGranted.java
@@ -0,0 +1,25 @@
+package miniproject.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.*;
+import miniproject.domain.*;
+import miniproject.infra.AbstractEvent;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class BookAccessGranted extends AbstractEvent {
+
+    private Long userId;
+    private String bookId;
+
+    public BookAccessGranted(Subscription aggregate) {
+        super(aggregate);
+    }
+
+    public BookAccessGranted() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
